a4cbd76a2d4a053207a178c90df50af2bf88a04b
emelansons <e6128687@cmaisonneuve.qc.ca>
2025-01-30 15:58:46 +0000

    printing nodes of a linked list

--- main.c
+++ main.c
@@ -5,15 +5,43 @@ struct Node {
     struct Node* next;
 };
 
+void printLinkedList (struct Node* head) {
+    struct Node current = *head;
+    int count = 0;
+    printf("START OF LIST\n");
+    printf("\tNODE: %d\t[ DATA:\t%d;\tNEXT:\t%p ]\n", count, current.data, current.next);
+    while (current.next) {
+        count++;
+        current = *current.next;
+        printf("\tNODE: %d\t[ DATA:\t%d;\tNEXT:\t%p ]\n", count, current.data, current.next);
+    }
+    printf("END OF LIST\n");
+}
+
+void addNode (struct Node* head, struct Node* newNode) {
+    struct Node current = *head;
+    while(current.next) {
+        current = *current.next;
+    }
+    printf("adding new node to the node with data = %d\n", current.data);
+    printf("%p\n", newNode);
+    current.next = newNode;
+
+}
+
 int main() {
     int x = 666, y = 333, z = 44;
-    int* ptr;
-    struct Node head = {x, NULL};
-    int* second[2] = {NULL, &y};
-    int* third[2] = {NULL, &z};
+    struct Node third = {0, NULL};
+    struct Node second = {2, &third};
+    struct Node head = {x, &second};
 
+    printLinkedList(&head);
+
+    struct Node newNode = {451, NULL};    
+    addNode(&head, &newNode);
+    printLinkedList(&head);
     
     
-    printf("Hello, World %d \n", head.data);
+    // printf("Hello, World %d \n", head.data);
     return 0;
 }
