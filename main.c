58d9903a3b14aa8407a3c4846d88854cd9f3006e
Melanson <e6128687@cmaisonneuve.qc.ca>
2025-01-30 12:24:53 -0500

    fixed my previous git mess... manually

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 struct Node {
-    int data;
+    char data[32];
     struct Node* next;
 };
 
@@ -9,35 +9,35 @@ void printLinkedList (struct Node* head) {
     struct Node current = *head;
     int count = 0;
     printf("START OF LIST\n");
-    printf("\tNODE: %d\t[ DATA:\t%d;\tNEXT:\t%p ]\n", count, current.data, current.next);
+    printf("\tNODE: %d\t[ DATA:%-32s;\tNEXT:\t%p ]\n", count, current.data, current.next);
     while (current.next) {
         count++;
         current = *current.next;
-        printf("\tNODE: %d\t[ DATA:\t%d;\tNEXT:\t%p ]\n", count, current.data, current.next);
+        printf("\tNODE: %d\t[ DATA:%-32s;\tNEXT:\t%p ]\n", count, current.data, current.next);
     }
     printf("END OF LIST\n");
 }
 
 void addNode (struct Node* head, struct Node* newNode) {
-    struct Node current = *head;
-    while(current.next) {
-        current = *current.next;
+    struct Node* current = head;
+    while(current->next) {
+        current = current->next;
     }
-    printf("adding new node to the node with data = %d\n", current.data);
+    printf("adding new node to the node with data = %s\n", current->data);
     printf("%p\n", newNode);
-    current.next = newNode;
+    current->next = newNode;
 
 }
 
 int main() {
     int x = 666, y = 333, z = 44;
-    struct Node third = {0, NULL};
-    struct Node second = {2, &third};
-    struct Node head = {x, &second};
+    struct Node third = {"", NULL};
+    struct Node second = {"2", &third};
+    struct Node head = {"HEAD", &second};
 
     printLinkedList(&head);
 
-    struct Node newNode = {451, NULL};    
+    struct Node newNode = {"Hello, World!", NULL};    
     addNode(&head, &newNode);
     printLinkedList(&head);
     
